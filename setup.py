378d5904312b9df871a510c9fb73582396be4256
Michael Nielsen <mn@michaelnielsen.org>
2012-06-29 21:33:51 -0400

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mini_qa',
+    version='0.01',
+    url='http://github.com/mnielsen/mini_qa/',
+    license='MIT',
+    author='Michael Nielsen',
+    author_email='mn@michaelnielsen.org',
+    description='A toy question-answering system',
+    platforms='any'
+    ]
+)
