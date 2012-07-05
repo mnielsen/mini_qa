7f958ec888ae55d76534b829f1894072882115d8
Michael Nielsen <mn@michaelnielsen.org>
2012-07-05 18:50:48 -0400

    Refactoring ec2.py so everything is done with shelves.

--- ec2.py
+++ ec2.py
@@ -5,7 +5,7 @@ Simple EC2 cluster management with Python.
 
 Mostly a convenience wrapper around the boto library.
 
-Useage from the command line:
+Usage from the command line:
 
     python ec2.py create CLUSTER_NAME n type 
 
@@ -72,7 +72,6 @@ import time
 
 # Third party libraries
 from boto.ec2.connection import EC2Connection
-import redis
 
 # The list of EC2 AMIs to use, from alestic.com
 AMIS = {"m1.small" : "ami-e2af508b",
@@ -105,125 +104,123 @@ check_environment_variables_exist("AWS_HOME", "AWS_KEYPAIR",
                                   "AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY")
 
 
-#### Check that the Redis key we need already exists
-r = redis.Redis(host='localhost', port=6379, db=0)
-if not r.exists("ec2_clusters"):
-    r.set("ec2_clusters", cPickle.dumps([]))
+# Persistent shelf, used to represent all the clusters.  The keys are
+# the cluster_names, and values represent cluster objects.  The
+# cluster objects are just lists of objects representing instances.
+# And instance objects are `dict`s with two keys, `id` and
+# `public_dns_name`, representing the Amazon EC2 `id` and the public
+# dns name, respectively.
+#
+# I considered implementing the cluster and instance objects using
+# Python classes.  However, this creates problems. The reason is that
+# the logic around persistence of a cluster or instance had to take
+# place _outside_ the class, while the logic around starting and
+# stopping EC2 instances took place within.  This separated the
+# persistence logic from the EC2 logic, which seemed like a bad idea.
+#
+# Note that clusters is a global object because it repreent a global
+# external state.
+clusters = shelve.open("ec2.shelf")
 
+ec2_conn = EC2Connection(os.environ["AWS_ACCESS_KEY_ID"], 
+                         os.environ["AWS_SECRET_ACCESS_KEY"])
 
-class Cluster():
-
-    def __init__(self, name):
-        self.name = name
-
-    def create(self, n, instance_type):
-        """
-        Create an EC2 cluster with name `self.name`, `n` instances, of
-        type `instance_type`.
-        """
-        r = redis.Redis(host='localhost', port=6379, db=0)
-        clusters = cPickle.loads(r.get("ec2_clusters"))
-        if self.name in clusters:
-            print "A cluster with name %s already exists.  Exiting."
-            sys.exit()
-        ec2_conn = EC2Connection(os.environ["AWS_ACCESS_KEY_ID"], 
-                                 os.environ["AWS_SECRET_ACCESS_KEY"])
-        try:
-            ami = AMIS[instance_type]
-        except:
-            ami = AMIS["m1.small"]
-        image = ec2_conn.get_all_images(image_ids=[ami])[0]
-        reservation = image.run(
-            n, n, os.environ["AWS_KEYPAIR"], instance_type=instance_type)
-        instances = reservation.instances
-        # Wait for the cluster to come up
-        for instance in instances:
-            while instance.update()== u'pending':
-                time.sleep(1)
-        # Reload, just in case anything has changed
-        clusters = cPickle.loads(r.get("ec2_clusters"))
-        instance_descriptions = [instance.public_dns_name for
-                                 instance in instances]
-        clusters.append(
-            (self.name, instance_descriptions))
-        r.set("ec2_clusters", cPickle.dumps(clusters))
-        # Give the ssh daemon time to start
-        time.sleep(120) 
-
-    def show(self):
-        print "Instances in cluster %s" % self.name
-        cluster = self.cluster_instances()
-        if cluster == None:
-            print "No cluster with that name.  Exiting."
-            sys.exit()
-        for (j, instance_dns) in enumerate(cluster):
-            print "%s: %s" % (j, instance_dns)
-
-    def shutdown(self):
-        running_instances = get_running_instances()
-        public_dns_names = [instance.public_dns_name 
-                            for instance in running_instances]
-        for public_dns_name in self.cluster_instances():
-            if public_dns_name in public_dns_names:
-                j = public_dns_names.index(public_dns_name)
-                instance = running_instances[j]
-                ec2_conn = EC2Connection(
-                    os.environ["AWS_ACCESS_KEY_ID"], 
-                    os.environ["AWS_SECRET_ACCESS_KEY"])
-                ec2_conn.terminate_instances([instance.id ])
-        r = redis.Redis(host='localhost', port=6379, db=0)
-        clusters = cPickle.loads(r.get("ec2_clusters"))
-        clusters = [(name, cluster) for (name, cluster) in clusters 
-                    if name != self.name]
-        r.set("ec2_clusters", cPickle.dumps(clusters))
-
-            
-    def cluster_instances(self):
-        r = redis.Redis(host='localhost', port=6379, db=0)
-        clusters = cPickle.loads(r.get("ec2_clusters"))
-        filtered_clusters = [cluster for (name, cluster) in clusters
-                             if name == self.name]
-        if len(filtered_clusters) == 0:
-            return None
-        else:
-            return filtered_clusters[0]
-
+def create(cluster_name, n, instance_type):
+    """
+    Create an EC2 cluster with name `cluster_name`, and `n` instances
+    of type `instance_type`.
+    """
+    # Parameter check
+    if cluster_name in clusters:
+        print "A cluster with name %s already exists.  Exiting."
+        sys.exit()
+    if n < 1 or n > 20:
+        print "Clusters must contain between 1 and 20 instances.  Exiting."
+        sys.exit()
+    if not instance_type in AMIS:
+        print "Instance type not recognized, setting it to be 'm1.small'."
+        instance_type = "m1.small"
+    ami = AMIS[instance_type]
+    # Create EC2 instances
+    image = ec2_conn.get_all_images(image_ids=[ami])[0]
+    reservation = image.run(
+        n, n, os.environ["AWS_KEYPAIR"], instance_type=instance_type)
+    for instance in reservation.instances:  # Wait for the cluster to come up
+        while instance.update()== u'pending':
+            time.sleep(1)
+    time.sleep(120) # Give the ssh daemon time to start
+    # Update clusters
+    cluster = [{"id": instance.id,
+                "public_dns_name": instance.public_dns_name}
+               for instance in reservation.instances]
+    clusters[cluster_name] = cluster
+    clusters.close()
 
 def show_all():
-    r = redis.Redis(host='localhost', port=6379, db=0)
-    clusters = cPickle.loads(r.get("ec2_clusters"))
     if len(clusters) == 0:
-        print "No clusters exist"
+        print "No clusters present."
     else:
-        print "Showing all clusters"
-        for (name, cluster) in clusters:
-            Cluster(name).show()
+        print "Showing all clusters."
+        for cluster_name in clusters:
+            show(cluster_name)
 
-def shutdown_all():
-    for name in get_cluster_names():
-        Cluster(name).shutdown()
+def show(cluster_name):
+    if cluster_name not in clusters:
+        print "No cluster with the name %s exists.  Exiting." % cluster_name
+        sys.exit()
+    print "Instances in cluster %s:" % cluster_name
+    cluster = clusters[cluster_name]
+    for (j, instance) in enumerate(cluster):
+        print "%s: %s" % (j, instance["public_dns_name"])
 
-def get_cluster_names():
-    r = redis.Redis(host='localhost', port=6379, db=0)
-    return [name for (name, cluster) in cPickle.loads(r.get("ec2_clusters"))]
+def shutdown(cluster_name):
+    if cluster_name not in clusters:
+        print "No cluster with the name %s exists.  Exiting." % cluster_name
+        sys.exit()
+    print "Shutting down cluster %s." % cluster_name
+    cluster = clusters[cluster_name]
+    cluster_public_dns_names = [instance["public_dns_name"]
+                                for instance in cluster]
+    all_running_instances = get_running_instances()
+    all_public_dns_names = [instance.public_dns_name 
+                            for instance in all_running_instances]
+    for public_dns_name in cluster_public_dns_names:
+        j = all_public_dns_names.index(public_dns_name)
+        instance = all_running_instances[j]
+        ec2_conn.terminate_instances([instance.id ])
+    del clusters[cluster_name]
+    clusters.close()
 
 def get_running_instances():
     """
     Return all the running EC2 instances.
     """
-    ec2_conn = EC2Connection(
-        os.environ["AWS_ACCESS_KEY_ID"], os.environ["AWS_SECRET_ACCESS_KEY"])
     reservations = ec2_conn.get_all_instances()
     running_instances = [instance for reservation in reservations
                          for instance in reservation.instances 
                          if instance.update() == u"running"]
     return running_instances
 
+def shutdown_all():
+    if len(clusters) == 0:
+        print "No clusters to shut down."
+    else:
+        for cluster_name in clusters:
+            shutdown(cluster_name)
 
-def login(instance):
+def login(cluster_name, instance_index):
     """
-    ssh to `instance`.
+    ssh to `instance_index` in `cluster_name`.
     """
+    if cluster_name not in clusters:
+        print "No cluster with the name %s exists.  Exiting." % cluster_name
+        sys.exit()
+    cluster = clusters[cluster_name]
+    try:
+        instance = cluster[instance_index]
+    except IndexError:
+        print ("The instance index must be in the range 0 to %s. Exiting." %
+               len(cluster)-1)
     print "SSHing to instance with address %s" % (instance.public_dns_name)
     keypair = "%s/%s.pem" % (os.environ["AWS_HOME"], os.environ["AWS_KEYPAIR"])
     os.system("ssh -i %s ubuntu@%s" % (keypair, instance.public_dns_name))
@@ -272,20 +269,20 @@ if __name__ == "__main__":
     cmd = sys.argv[1]
     l = len(args)
     if cmd=="create" and l==4:
-        cluster = Cluster(args[1])
-        cluster.create(int(args[2]), args[3])
+        create(args[1], int(args[2]), args[3])
     elif cmd=="show_all" and l==1:
         show_all()
     elif cmd=="show" and l==2:
-        cluster = Cluster(args[1])
-        cluster.show()
+        show(args[1])
     elif cmd=="shutdown" and l==2:
-        cluster = Cluster(args[1])
-        cluster.shutdown()
-    elif cmd=="shutdown_all" and l==3:
+        shutdown(args[1])
+    elif cmd=="shutdown_all" and l==1:
         shutdown_all()
+    elif cmd=="login" and l==2:
+        login(args[1], 0)
+    elif cmd=="login" and l==3:
+        login(args[1], int(args[2]))
     elif cmd=="ssh" and (l==2 or l==3):
-        cluster = Cluster(args[1])
-        cluster.ssh(args[2:])
+        cluster.ssh(args[1:])
     else:
-        print self.__doc__()
+        print __doc__
